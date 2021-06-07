31d096cd3dcb53763dbf99b97e3b9a93ef2929a2
iMac <macoscatalina@iMac.broadband>
2021-06-07 16:18:44 +0100

    first commit

---
+++ Hello.js
@@ -0,0 +1,109 @@
+// // console.log("Hello".length);
+// // console.log("Hello".toUpperCase());
+// // console.log(Math.random()*10);
+// // console.log ("All Around the world".charAt(7).toUpperCase());
+// // // console.log("All Around the world".toUpperCase().charAt(7));
+// // let favouriteDrink = "coffee";
+// // console.log(favouriteDrink);
+// // let favouriteDrink = "coffee";
+// // console.log('My favourite drink is' + ${favouriteDrink'});
+
+// // // if (condition1) {//do this} else if (condition2) {//do this} else {//if nothing else matched do this}
+// // let music = "classical";
+// // if (music =="classical") {console.log("oh no it's that classical again");}
+// // 1==1
+// // let car = "Peugeot";
+// // if(car == "Ford" || car == "GM"){
+// //     console.log("You've got an American car!");
+// //     }
+// // let age = 18;
+// // if (age >= 18){console.log("Heres a beer");} else{
+// // console.log("Enjoy your J20");}
+// // 17 =="UK"
+
+// // let weird = 11;
+// // if (weird & 1) {
+// // console.log("It's odd");
+// // } else {
+// // console.log("It's even");
+// // }
+// // "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuh gtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi"
+
+// // array1 = ["h","e", "l", "l", "0"];
+
+// // array2 = ["h","e", "l", "l", "0"];
+
+// // let ageC = 16;
+// // let child = 8 ;
+// // let adult = 10.95;
+// // let senior = 7.50;
+
+// // if (ageC < 18 ) {
+// // console.log(`child price is ${child} pounds`);
+// // }
+// // else if (ageC >18 && ageC <=60) {
+// // console.log(`adult price is ${adult} pounds`);
+// // }
+// // else {
+// // console.log(`senior price is ${senior}`);
+// // }
+// // let age = 10;
+// // if (age<18){
+// // console.log("Child ticket is £8")
+// // }
+// // else if (age>=18 && age<60){
+// // console.log("Adult ticket is £10.95")
+// // }
+// // else if (age>=60){
+// // console.log("Senior ticket is £7.50")
+// // }
+// // const pressGrindBeans = () => {
+
+// //     const pressGrindBeans = () => {
+// //         console.log("Grinding for 20 seconds");
+// //         }
+// //         // pressGrindBeans();
+
+// // const pressGrindBeans = () => {
+// //     console.log("Grinding for 20 seconds");
+// //     }
+// //     pressGrindBeans();
+
+//     constcashWithdrawal = (amount, accnum) => {
+//         console.log(`Withdrawing ${amount} from account ${accnum}`);
+//         }
+//         onstcashWithdrawal = (amount, accnum) => {
+//             console.log(`Withdrawing ${amount} from account ${accnum}`);
+//             }
+//             cashWithdrawal(300, 50449921);
+//             cashWithdrawal(30, 50449921);
+//             cashWithdrawal(200, 50447921);
+// let account = 50449921
+
+// const cashWithdrawl =(amount, accoum) => {
+
+// const addUp = (num1, num2) => {
+//         let sum = num1 + num2;
+//         console.log(sum)
+//         }
+//         addUp(22, 10);
+
+const multiplyByNineFifths = (celsius) => {
+        return celsius * (9/5);
+        };
+
+        const getFahrenheit = (celsius) => {
+                return multiplyByNineFifths(celsius) + 32;
+                };
+
+                const multiplyByNineFifths = (celsius) => {
+                        return celsius * (9/5);
+                        };
+                        const getFahrenheit = (celsius) => {
+                        return multiplyByNineFifths(celsius) + 32;
+                        };
+                        console.log("The temperature is " + getFahrenheit(15) + "°F");// Output: The temperature is 59°F
+                        
+let orderCount = 0;
+const takeOrder = (topping0 => {
+        
